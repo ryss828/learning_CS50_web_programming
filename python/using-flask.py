7ed2a4eeae09258b49ffdbb0d6aa4d2b3aacf741
ryss <ryusasai@gmail.com>
2020-05-15 09:55:48 +0100

    ..

--- python/using-flask.py
+++ python/using-flask.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "web server by Python flask"
+
+@app.route("/store")
+def store():
+    return "web server by Python flask: it's a store here"
