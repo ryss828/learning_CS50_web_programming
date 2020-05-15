b5a77fb16babe56ce7d7e71344c644239ced814e
ryss <ryusasai@gmail.com>
2020-05-15 15:57:03 +0100

    ..

--- python/using-flask-template.py
+++ python/using-flask-template.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    hl = "Hello world!"
+    return render_template("index.html")
+
+@app.route("/<string:specified>")
+def headline_specified(specified):
+    hl = specified.capitalize()
     return render_template("index.html", headline=hl)
