e293af21128f3e265fdc45d63e8633917d20895d
ryss <ryusasai@gmail.com>
2020-05-16 17:17:00 +0100

    .

--- python/using-flask-template.py
+++ python/using-flask-template.py
@@ -12,6 +12,6 @@ def headline_specified(specified):
     return render_template("index.html", headline=hl)
 
 @app.route("/array")
-def arary_template():
+def array_template():
     these = ["Ryu", "Emma", "Luka", "Hugo"]
     return render_template("array-handling.html", names=these)
